62665f77c106a1149a44b89ae2911be07a51ed11
kadiriayman96 <ayman.kadev@gmail.com>
2024-05-15 16:24:22 +0100

    Move Mailer.js to Helpers

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -1,14 +1,12 @@
 import { homeController, bookNow } from "../controllers/homeController.js";
-import { sendEmail } from "../middlewares/mailer.js";
+import { sendEmail } from "../helpers/mailer.js";
 import { validationReservation } from "../middlewares/validationResa.js";
 import express from "express";
 
 const homeRouter = express.Router();
 
 homeRouter.get("/", homeController);
-
 homeRouter.post("/sendEmail", sendEmail);
-
 homeRouter.post("/bookNow", validationReservation, bookNow);
 
 export { homeRouter };
